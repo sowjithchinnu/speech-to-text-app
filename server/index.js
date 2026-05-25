00cbd4bcb290d92d5c890f2f2288338282a455ec
Your Name <yourmail@gmail.com>
2026-05-25 22:41:24 +0530

    frontend deployment

--- server/index.js
+++ server/index.js
@@ -87,4 +87,6 @@ app.delete("/transcriptions/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const PORT = process.env.PORT || 3000;
